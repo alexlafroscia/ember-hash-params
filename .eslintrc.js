69ccda2719c8c8542eb2a01c3dd787262af4c843
Alex LaFroscia <alex@lafroscia.com>
2017-08-04 20:28:25 -0700

    Install/configure Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,19 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'prettier'
+  ],
   env: {
     browser: true
   },
   rules: {
+    'prettier/prettier': ['error', {
+      singleQuote: true
+    }]
   }
 };
